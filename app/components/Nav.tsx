0fc4625ce0e5d52019fc3147e855323c85b99249
Renzo Cardoza <car22001@byui.edu>
2026-04-14 22:13:41 -0300

    added artisans page connection and button to the nav. changed the ui for artisans page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,6 +6,7 @@ export default function Nav() {
     <nav className="flex justify-center font-body space-x-8 bg-gray-100 py-2">
       <Link href="/" className="hover:text-green-600">Home</Link>
       <Link href="/products" className="hover:text-green-600">Shop</Link>
+      <Link href="/artisans" className="hover:text-green-600">Artisans</Link>
       <Link href="/sell" className="hover:text-green-600">Sell</Link>
     </nav>
   );
